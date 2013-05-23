f5c358e0a2a5a27135802a560eb828d326f9d757
Roberto Spadim <roberto@spadim.com.br>
2013-05-23 20:13:06 -0300

    Changed prepend to don't run if function don't exists and if in cli

--- inc/prepend.php
+++ inc/prepend.php
@@ -1,8 +1,4 @@
 <?php
 // currently not supported
-if(php_sapi_name() == 'cli')
-{
-	return;
-}
-
-xhprof_enable(XHPROF_FLAGS_MEMORY | XHPROF_FLAGS_CPU);
+if (php_sapi_name() !== 'cli' && function_exists('xhprof_enable'))
+	xhprof_enable(XHPROF_FLAGS_MEMORY | XHPROF_FLAGS_CPU);
